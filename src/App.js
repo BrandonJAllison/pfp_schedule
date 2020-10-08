b8634b5019b8cec438387db0d146a2fa18c275a9
Brandon Allison <brandonallison@pop-os.localdomain>
2020-10-08 08:32:07 -0400

    building again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Login from './Login'
+// import Login from './Login'
 import Scheduler from './Scheduler'
 
 
@@ -8,8 +8,8 @@ const App = () => {
   return (
    
      <div>
-      <Login/> 
-       {/* <Scheduler/> */}
+      {/* <Login/>  */}
+       <Scheduler/>
      </div>
     
   );
